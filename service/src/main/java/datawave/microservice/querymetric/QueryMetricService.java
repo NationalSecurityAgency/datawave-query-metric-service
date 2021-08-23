450295f2bcac3c43d97dfbee68527587873a405a
Bill Oley <bill.oley@gmail.com>
2021-08-23 16:09:04 -0400

    Updates to get consul discovery working correctly for multiple instances
    add unique instance id for consul profile to bootstrap.yml

--- service/src/main/java/datawave/microservice/querymetric/QueryMetricService.java
+++ service/src/main/java/datawave/microservice/querymetric/QueryMetricService.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.commons.util.InetUtilsProperties;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 /**
  * Launcher for the query metric service.
@@ -11,7 +16,31 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"datawave.microservice", "datawave.query.util"}, exclude = {ErrorMvcAutoConfiguration.class})
 public class QueryMetricService {
+    private static final int MAX_PORT = 100;
+    
+    static {
+        System.setProperty("hazelcast.cluster.host", findHostInfo().getIpAddress());
+        System.setProperty("hazelcast.cluster.port", String.valueOf(getNextPort(5701)));
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(QueryMetricService.class, args);
+    }
+    
+    private static int getNextPort(int start) {
+        for (int port = start; port < start + MAX_PORT; ++port) {
+            try {
+                new ServerSocket(port).close();
+                return port;
+            } catch (IOException e) {
+                
+            }
+        }
+        return -1;
+    }
+    
+    private static InetUtils.HostInfo findHostInfo() {
+        InetUtils inetUtils = new InetUtils(new InetUtilsProperties());
+        return inetUtils.findFirstNonLoopbackHostInfo();
     }
 }
