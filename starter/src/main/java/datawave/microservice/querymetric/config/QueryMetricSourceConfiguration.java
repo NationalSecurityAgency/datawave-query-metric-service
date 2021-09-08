264b42bcc1545cff62b384d7e26dc03c40c2b5aa
Bill Oley <bill.oley@gmail.com>
2021-09-08 11:26:20 -0400

    Updated the configuration to support independent query metrics source and sink enable/disable properties

--- starter/src/main/java/datawave/microservice/querymetric/config/QueryMetricSourceConfiguration.java
+++ starter/src/main/java/datawave/microservice/querymetric/config/QueryMetricSourceConfiguration.java
@@ -1,10 +1,14 @@
 package datawave.microservice.querymetric.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.SubscribableChannel;
 
 @Configuration
+@EnableBinding({QueryMetricSourceConfiguration.QueryMetricSourceBinding.class})
+@ConditionalOnProperty(name = "datawave.query.metric.client.source.enabled", havingValue = "true", matchIfMissing = true)
 public class QueryMetricSourceConfiguration {
     
     public interface QueryMetricSourceBinding {
