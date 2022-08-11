e9db34aac9cb867dc3e00134ad50d904f3f4802f
Kevin Rathbun <kevinrr888@gmail.com>
2022-08-11 13:57:07 -0400

    Changes:
    - QueryMetricsDetailListResponse no longer uses HtmlProvider and a StringBuilder to produce the HTML. Added new method queryWebpage() to QueryMetricOperations which handles HTML responses for the Query Metrics webpage (response uses new detailedquerymetrics.html template file).
    - Added dependencies for jQuery in service/pom.xml and fixed src tags of scripts using jquery.min.js to link to the correct location of jquery.min.js (originally wrong link and jQuery wasn't a dependency)
    - Added thymeleaf dependency to service/pom.xml

--- service/src/main/java/datawave/microservice/querymetric/QueryMetricOperations.java
+++ service/src/main/java/datawave/microservice/querymetric/QueryMetricOperations.java
@@ -6,6 +6,8 @@ import com.hazelcast.spring.cache.HazelcastCacheManager;
 import datawave.marking.MarkingFunctions;
 import datawave.microservice.authorization.user.ProxiedUserDetails;
 import datawave.microservice.querymetric.BaseQueryMetric.Lifecycle;
+import datawave.microservice.querymetric.BaseQueryMetric.PageMetric;
+import datawave.microservice.querymetric.BaseQueryMetric.Prediction;
 import datawave.microservice.querymetric.config.TimelyProperties;
 import datawave.microservice.querymetric.factory.BaseQueryMetricListResponseFactory;
 import datawave.microservice.querymetric.function.QueryMetricSupplier;
@@ -29,6 +31,7 @@ import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.security.ColumnVisibility;
 import org.apache.accumulo.core.security.VisibilityEvaluator;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +47,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.security.PermitAll;
 import javax.inject.Named;
@@ -58,7 +62,9 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TimeZone;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -313,8 +319,7 @@ public class QueryMetricOperations {
      */
     @Operation(summary = "Get the metrics for a given query ID.")
     @PermitAll
-    @RequestMapping(path = "/id/{queryId}", method = {RequestMethod.GET},
-                    produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_HTML_VALUE})
+    @RequestMapping(path = "/id/{queryId}", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public BaseQueryMetricListResponse query(@AuthenticationPrincipal ProxiedUserDetails currentUser,
                     @Parameter(description = "queryId to return") @PathVariable("queryId") String queryId) {
         
@@ -362,6 +367,152 @@ public class QueryMetricOperations {
             response.setHasResults(true);
         }
         return response;
+    }
+    
+    /**
+     * Returns metrics for the current users queries that are identified by the id
+     *
+     * @param currentUser
+     *            the current user
+     * @param queryId
+     *            the query id
+     * @return the ModelAndView for the webpage
+     * @HTTP 200 success
+     * @HTTP 500 internal server error
+     */
+    @Operation(summary = "Get the metrics for a given query ID.")
+    @PermitAll
+    @RequestMapping(path = "/id/{queryId}", method = {RequestMethod.GET}, produces = {MediaType.TEXT_HTML_VALUE})
+    public ModelAndView queryWebpage(@AuthenticationPrincipal ProxiedUserDetails currentUser,
+                    @Parameter(description = "queryId to return") @PathVariable("queryId") String queryId) {
+        
+        BaseQueryMetricListResponse response = query(currentUser, queryId);
+        ModelAndView mav = new ModelAndView();
+        
+        mav.setViewName("detailedquerymetrics");
+        
+        TreeMap<Date,QueryMetric> metricMap = new TreeMap<Date,QueryMetric>(Collections.reverseOrder());
+        List<QueryMetric> responseResult = response.getResult();
+        
+        for (QueryMetric metric : responseResult) {
+            metricMap.put(metric.getCreateDate(), metric);
+        }
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HHmmss");
+        
+        List<List<String>> metricsTableContent = new ArrayList<List<String>>();
+        List<List<String>> pageTimesTableContent = new ArrayList<List<String>>();
+        for (QueryMetric metric : metricMap.values()) {
+            List<String> metricsData = new ArrayList<String>();
+            Set<datawave.webservice.query.QueryImpl.Parameter> parameters = metric.getParameters();
+            
+            metricsData.add(metric.getColumnVisibility());
+            metricsData.add(sdf.format(metric.getCreateDate()));
+            metricsData.add(metric.getUser());
+            String userDN = metric.getUserDN();
+            metricsData.add(userDN == null ? "" : userDN);
+            String proxyServers = metric.getProxyServers() == null ? "" : StringUtils.join(metric.getProxyServers(), "<BR/>");
+            metricsData.add(proxyServers);
+            metricsData.add(metric.getQueryId());
+            metricsData.add(metric.getQueryType());
+            metricsData.add(metric.getQueryLogic());
+            String queryStyle = QueryMetricsDetailListResponse.isJexlQuery(parameters) ? "white-space: pre; word-wrap: break-word;" : "word-wrap: break-word;";
+            metricsData.add(queryStyle);
+            metricsData.add(metric.getQuery());
+            metricsData.add(metric.getPlan());
+            metricsData.add(metric.getQueryName());
+            String beginDate = metric.getBeginDate() == null ? "" : sdf.format(metric.getBeginDate());
+            metricsData.add(beginDate);
+            String endDate = metric.getEndDate() == null ? "" : sdf.format(metric.getEndDate());
+            metricsData.add(endDate);
+            metricsData.add(parameters == null ? "" : QueryMetricsDetailListResponse.toFormattedParametersString(parameters));
+            String queryAuths = metric.getQueryAuthorizations() == null ? "" : metric.getQueryAuthorizations().replaceAll(",", " ");
+            metricsData.add(queryAuths);
+            metricsData.add(metric.getHost());
+            
+            StringBuilder sbPredictions = new StringBuilder();
+            if (metric.getPredictions() != null && !metric.getPredictions().isEmpty()) {
+                String delimiter = "";
+                List<Prediction> predictions = new ArrayList<Prediction>(metric.getPredictions());
+                Collections.sort(predictions);
+                for (Prediction prediction : predictions) {
+                    sbPredictions.append(delimiter).append(prediction.getName()).append(" = ").append(prediction.getPrediction());
+                    delimiter = "<br>";
+                }
+            } else {
+                sbPredictions.append("");
+            }
+            metricsData.add(sbPredictions.toString());
+            
+            metricsData.add(QueryMetricsDetailListResponse.numToString(metric.getLoginTime(), 0));
+            metricsData.add(String.valueOf(metric.getSetupTime()));
+            metricsData.add(QueryMetricsDetailListResponse.numToString(metric.getCreateCallTime(), 0));
+            metricsData.add(String.valueOf(metric.getNumPages()));
+            metricsData.add(String.valueOf(metric.getNumResults()));
+            metricsData.add(String.valueOf(metric.getDocRanges()));
+            metricsData.add(String.valueOf(metric.getFiRanges()));
+            metricsData.add(String.valueOf(metric.getSourceCount()));
+            metricsData.add(String.valueOf(metric.getNextCount()));
+            metricsData.add(String.valueOf(metric.getSeekCount()));
+            metricsData.add(String.valueOf(metric.getYieldCount()));
+            metricsData.add(metric.getVersion());
+            
+            long count = 0l;
+            long callTime = 0l;
+            long serializationTime = 0l;
+            long bytesSent = 0l;
+            for (PageMetric p : metric.getPageTimes()) {
+                List<String> pageTimesData = new ArrayList<String>();
+                
+                count += p.getReturnTime();
+                callTime += (p.getCallTime()) == -1 ? 0 : p.getCallTime();
+                serializationTime += (p.getSerializationTime()) == -1 ? 0 : p.getSerializationTime();
+                bytesSent += (p.getBytesWritten()) == -1 ? 0 : p.getBytesWritten();
+                
+                String pageRequestedStr = "";
+                long pageRequested = p.getPageRequested();
+                if (pageRequested > 0) {
+                    pageRequestedStr = sdf.format(new Date(pageRequested));
+                }
+                
+                String pageReturnedStr = "";
+                long pageReturned = p.getPageReturned();
+                if (pageReturned > 0) {
+                    pageReturnedStr = sdf.format(new Date(pageReturned));
+                }
+                
+                pageTimesData.add(QueryMetricsDetailListResponse.numToString(p.getPageNumber(), 1));
+                pageTimesData.add(pageRequestedStr);
+                pageTimesData.add(pageReturnedStr);
+                pageTimesData.add(String.valueOf(p.getReturnTime()));
+                pageTimesData.add(String.valueOf(p.getPagesize()));
+                pageTimesData.add(QueryMetricsDetailListResponse.numToString(p.getCallTime(), 0));
+                pageTimesData.add(QueryMetricsDetailListResponse.numToString(p.getLoginTime(), 0));
+                pageTimesData.add(QueryMetricsDetailListResponse.numToString(p.getSerializationTime(), 0));
+                pageTimesData.add(QueryMetricsDetailListResponse.numToString(p.getBytesWritten(), 0));
+                
+                pageTimesTableContent.add(pageTimesData);
+            }
+            
+            metricsData.add(String.valueOf(count));
+            metricsData.add(QueryMetricsDetailListResponse.numToString(callTime, 0));
+            metricsData.add(QueryMetricsDetailListResponse.numToString(serializationTime, 0));
+            metricsData.add(QueryMetricsDetailListResponse.numToString(bytesSent, 0));
+            metricsData.add(String.valueOf(metric.getLifecycle()));
+            metricsData.add(String.valueOf(metric.getElapsedTime()));
+            String errorCode = metric.getErrorCode();
+            metricsData.add((errorCode == null) ? "" : StringEscapeUtils.escapeHtml4(errorCode));
+            String errorMessage = metric.getErrorMessage();
+            metricsData.add((errorMessage == null) ? "" : StringEscapeUtils.escapeHtml4(errorMessage));
+            
+            metricsTableContent.add(metricsData);
+        }
+        
+        mav.addObject("isGeoQuery", response.isGeoQuery());
+        mav.addObject("metricsTableContent", metricsTableContent);
+        mav.addObject("pageTimesTableContent", pageTimesTableContent);
+        
+        return mav;
     }
     
     /**
