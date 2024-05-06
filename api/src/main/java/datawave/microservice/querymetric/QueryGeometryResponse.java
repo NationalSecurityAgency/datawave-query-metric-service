1b56d17189eff16d3cbd4a246454a2d9119ec8ea
Kevin Rathbun <43969518+kevinrr888@users.noreply.github.com>
2024-05-06 13:46:33 -0400

    Update query-metric service to generate HTML response via Spring MVC (#6)

--- api/src/main/java/datawave/microservice/querymetric/QueryGeometryResponse.java
+++ api/src/main/java/datawave/microservice/querymetric/QueryGeometryResponse.java
@@ -1,8 +1,6 @@
 package datawave.microservice.querymetric;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.xml.bind.annotation.XmlAccessOrder;
@@ -14,9 +12,10 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
+import org.springframework.web.servlet.ModelAndView;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import datawave.webservice.HtmlProvider;
 import datawave.webservice.result.BaseResponse;
 
 /**
@@ -25,37 +24,33 @@ import datawave.webservice.result.BaseResponse;
 @XmlRootElement(name = "QueryGeometry")
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
-public class QueryGeometryResponse extends BaseResponse implements HtmlProvider {
+public class QueryGeometryResponse extends BaseResponse {
     
     private static final long serialVersionUID = 1L;
     
-    private static final String TITLE = "Query Geometry";
+    @XmlTransient
+    protected String header;
+    @XmlTransient
+    protected String footer;
     
-    private String LEAFLET_INCLUDES;
-    private String JQUERY_INCLUDES;
-    private String MAP_INCLUDES;
+    protected String basePath = "/querymetric";
     
-    public QueryGeometryResponse() {
-        this(null, null);
+    public QueryGeometryResponse() {}
+    
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
     }
     
-    public QueryGeometryResponse(String queryId, String basemaps) {
-        this.queryId = queryId;
-        this.basemaps = basemaps;
-        setHtmlIncludePaths(new HashMap<>());
-    }
-    
-    public void setHtmlIncludePaths(Map<String,String> pathMap) {
-        // @formatter:off
-        LEAFLET_INCLUDES =
-                "<link rel='stylesheet' type='text/css' href='" + pathMap.getOrDefault("leaflet", "") + "/leaflet.css' />\n" +
-                        "<script type='text/javascript' src='" + pathMap.getOrDefault("leaflet", "") + "/leaflet.js'></script>\n";
-        JQUERY_INCLUDES =
-                "<script type='text/javascript' src='" + pathMap.getOrDefault("jquery", "") + "/jquery.min.js'></script>\n";
-        MAP_INCLUDES =
-                "<link rel='stylesheet' type='text/css' href='" + pathMap.getOrDefault("css", "") + "/queryMap.css' />\n" +
-                        "<script type='text/javascript' src='" + pathMap.getOrDefault("js", "") + "/queryMap.js'></script>";
-        // @formatter:on
+    public ModelAndView createModelAndView() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("querymetricgeometry");
+        mav.addObject("basemapScript", "var basemaps = " + this.basemaps + ";");
+        mav.addObject("geoJsonFeaturesScript", "var features = " + toGeoJsonFeatures() + ";");
+        mav.addObject("basePath", this.basePath);
+        mav.addObject("queryId", getQueryId());
+        mav.addObject("header", header);
+        mav.addObject("footer", footer);
+        return mav;
     }
     
     @XmlElement(name = "queryId", nillable = true)
@@ -68,38 +63,6 @@ public class QueryGeometryResponse extends BaseResponse implements HtmlProvider 
     @XmlElementWrapper(name = "features")
     @XmlElement(name = "feature")
     protected List<QueryGeometry> result = null;
-    
-    @JsonIgnore
-    @XmlTransient
-    @Override
-    public String getTitle() {
-        if (queryId != null)
-            return TITLE + " - " + queryId;
-        return TITLE;
-    }
-    
-    @JsonIgnore
-    @XmlTransient
-    @Override
-    public String getHeadContent() {
-        String basemapData = "<script type='text/javascript'>var basemaps = " + basemaps + ";</script>\n";
-        String featureData = "<script type='text/javascript'>var features = " + toGeoJsonFeatures() + ";</script>\n";
-        return String.join("\n", featureData, JQUERY_INCLUDES, LEAFLET_INCLUDES, basemapData, MAP_INCLUDES);
-    }
-    
-    @JsonIgnore
-    @XmlTransient
-    @Override
-    public String getPageHeader() {
-        return getTitle();
-    }
-    
-    @JsonIgnore
-    @XmlTransient
-    @Override
-    public String getMainContent() {
-        return "<div align='center'><div id='map' style='height: calc(100% - 85px); width: 100%; position: fixed; top: 86px; left: 0px;'></div></div>";
-    }
     
     private String toGeoJsonFeatures() {
         if (!this.result.isEmpty())
@@ -130,5 +93,13 @@ public class QueryGeometryResponse extends BaseResponse implements HtmlProvider 
     
     public void setBasemaps(String basemaps) {
         this.basemaps = basemaps;
+    }
+    
+    public void setHeader(String header) {
+        this.header = header;
+    }
+    
+    public void setFooter(String footer) {
+        this.footer = footer;
     }
 }
